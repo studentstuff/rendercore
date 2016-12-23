8a23ed52c74589ee2c71715efd423d852c2f03ba
strelkovsky <strelkin7@gmail.com>
2016-12-23 16:38:39 +0300

    VertexFormat draft

--- VertexArray.h
+++ VertexArray.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <glm.hpp>
 #include <memory>
 
@@ -15,6 +16,99 @@ struct Vertex
 		m_pos = pos;
 		m_tex = tex;
 	}
+};
+
+enum struct ElementType : uint16_t
+{
+	Float,
+	Float2,
+	Float3,
+	Float4
+};
+
+enum struct ElementUsage : uint16_t
+{
+	Position,
+	Uv
+};
+
+struct VertexComponent
+{
+	VertexComponent();
+	VertexComponent(ElementUsage u, ElementType t) : usage(u), type(t) {}
+
+	ElementUsage Usage() const { return usage; }
+	ElementType Type() const { return type; }
+	int32_t GetByteSize() const 
+	{ 
+		switch (type)
+		{
+		case ElementType::Float:
+			return 4;
+			break;
+		case ElementType::Float2:
+			return 8;
+			break;
+		case ElementType::Float3:
+			return 12;
+			break;
+		case ElementType::Float4:
+			return 16;
+			break;
+		default:
+			break;
+		}
+	}
+
+private:
+	ElementUsage usage;
+	ElementType type;
+};
+
+struct VertexFormat
+{
+	VertexFormat() {}
+
+	void Add(const VertexComponent& comp)
+	{
+		int32_t num = this->GetNumComponents();
+		comps[num++] = comp;
+	}
+	void Add(ElementUsage u, ElementType t)
+	{
+		Add(VertexComponent(u, t));
+	}
+
+	int32_t GetNumComponents() const { return num; }
+	const VertexComponent& GetComponent(int32_t index) const { return comps[index]; }
+
+	int32_t GetByteSize() const
+	{
+		int32_t byteSize = 0;
+		for (int32_t i = 0; i < num; i++) 
+		{
+			byteSize += comps[i].GetByteSize();
+		}
+		return byteSize;
+	}
+	int32_t GetComponentByteOffset(int32_t index) const
+	{
+		int32_t byteOffset = 0;
+		for (int32_t i = 0; i < index; i++) 
+		{
+			byteOffset += comps[i].GetByteSize();
+		}
+		return byteOffset;
+	}
+
+private:
+	enum
+	{
+		MaxNumVertexComponents
+	};
+
+	std::array<VertexComponent, MaxNumVertexComponents> comps;
+	int32_t num;
 };
 
 struct VertexArray
